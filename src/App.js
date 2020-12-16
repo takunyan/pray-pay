d77fd8b82bc6e9acc370622baee8ddcb73c48263
Navi <navi@Navis-MacBook-Pro.local>
2020-12-16 18:12:03 +0900

    problems with merge part2

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ import About from "./components/About.js";
 
 //firebase
 import firebase from "./firebase";
-import 'firebase/functions';
+import "firebase/functions";
 
 function App() {
   function testCF() {
-    const testFunc = firebase.functions().httpsCallable('test');
+    const testFunc = firebase.functions().httpsCallable("test");
     const messageText = "cc16";
     testFunc({ text: messageText }).then(function (result) {
       // Read result of the Cloud Function.
@@ -26,14 +26,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-<<<<<<< HEAD
+      <button onClick={testCF}>Test cloud functions</button>
+
       <Router className="Router">
-=======
-      <button onClick={testCF}>
-        Test cloud functions
-    </button>
-      <Router>
->>>>>>> 7f9f0e17710ba0bd8fa0b8cb9e0160ed639c38d3
         <Switch>
           <Route exact path="/" component={Main} />
           <Route exact path="/Canceled" component={Canceled} />
