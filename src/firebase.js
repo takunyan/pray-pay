5bddb73d512dc32ee775b03e1b3bf4d050bf5fd3
taniyamatakurou <pochomukinsan@gmail.com>
2020-12-16 15:16:11 +0900

    created an example of calling cloud functions

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase/app';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAWuYXOtJ9bAFiZVR3e9Yldh54tY_TTKZ4",
+    authDomain: "pray-pay.web.app",
+    projectId: "pray-pay",
+    appId: "1:886952026191:web:f94cb106df3c8b08c98a3a",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase
