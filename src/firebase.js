20c1326af5974d595b0d78254784ef9cdbf1f566
taniyamatakurou <pochomukinsan@gmail.com>
2020-12-16 17:05:41 +0900

    created .env

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import * as firebase from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAWuYXOtJ9bAFiZVR3e9Yldh54tY_TTKZ4",
+    apiKey: process.env.REACT_APP_API_KEY,
     authDomain: "pray-pay.web.app",
     projectId: "pray-pay",
     appId: "1:886952026191:web:f94cb106df3c8b08c98a3a",
