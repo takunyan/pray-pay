8c9429fc56c5027d0e53599f0354186bd329b81e
AVStevenson <alexandervikenstevenson@gmail.com>
2020-12-17 17:57:35 +0900

    fixing git issues2

--- src/components/Success.js
+++ src/components/Success.js
@@ -2,16 +2,13 @@ import React from "react";
 import logo from "../image/success.png";
 import { Route } from "react-router-dom";
 import Button from "@material-ui/core/Button";
-<<<<<<< HEAD
-=======
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   margin: {
     margin: theme.spacing(1),
-  }
+  },
 }));
->>>>>>> ae7d2956a2eddbe6b02d0de617d8674bb72f351f
 
 export default function Success() {
   const classes = useStyles();
@@ -24,15 +21,10 @@ export default function Success() {
         <Route
           render={({ history }) => (
             <Button
-<<<<<<< HEAD
-              className="aboutbutton"
-              type="button"
-=======
               variant="outlined"
               color="secondary"
               size="large"
               className={classes.margin}
->>>>>>> ae7d2956a2eddbe6b02d0de617d8674bb72f351f
               onClick={() => {
                 history.push("/");
               }}
