bf8f7a60825fe8e981a11764bb6d95c74d439353
Hidehiro Aya <hi.dehiro424@gmail.com>
2020-12-17 20:22:24 +0900

    added test function for Omikuji

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -22,8 +22,8 @@ exports.getCheckoutOmikuji = functions.https.onCall(async (data, context) => {
         { price: "price_1HzIhWHh1P19FUEPwn7orbkc", quantity: 1},
       ],
       mode: "payment",
-      success_url: "http://localhost:3000/Success",
-      cancel_url: "http://localhost:3000/Canceled",
+      success_url: "https://pray-pay.web.app/OmikujiSuccess",
+      cancel_url: "https://pray-pay.web.app/OmikujiCanceled",
     });
   }
 );
@@ -41,4 +41,18 @@ exports.getTestCheckoutSession = functions.https.onCall(
       cancel_url: "http://localhost:3000/Canceled",
     });
   }
+);
+
+
+exports.getTestCheckoutOmikuji = functions.https.onCall(async (data, context) => {
+  return await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      { price: "price_1HzIhWHh1P19FUEPwn7orbkc", quantity: 1},
+    ],
+    mode: "payment",
+    success_url: "http://localhost:3000/OmikujiSuccess",
+    cancel_url: "http://localhost:3000/OmikujiCanceled",
+  });
+}
 );
